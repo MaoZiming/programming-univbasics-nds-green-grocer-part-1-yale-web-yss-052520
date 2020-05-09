5cdcdcff54a64016082f2a7b1f017f93cceccbf2
Ziming Mao <ziming.mao@yale.edu>
2020-05-09 00:57:02 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,6 +2,13 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  counter = 0;
+  while counter < collection.size do
+    if collection[counter]{:item} == name
+      return collection[counter]
+    counter += 1
+  end
+  return nil;
 
 end
 
